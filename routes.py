028052916536deb159e32cfb2f6d3443f7a7943e
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2019-11-11 13:14:03 -0800

    Formatting

--- routes.py
+++ routes.py
@@ -10,17 +10,17 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
-  name = request.json['name']
+  # name = request.json['name']
   email = request.json['email']
   password = request.json['password']
 
   msg = ""
   if not email or not password:
-    msg = {"status": {"type": "failure",   "message": "Missing Data"}}
+    msg = {"status": {"type": "failure", "message": "Missing Data"}}
     return jsonify(msg)
 
   user = User.query.filter_by(email=email).first()
-  print(user.name)
+
   if user is None or not user.check_password(password):
     msg = {"status": {"type": "failure",
                       "message": "Username or password incorrect"}}
@@ -41,11 +41,11 @@ def register():
 
   msg = ""
   if not name or not email or not password:
-    msg = {"status": {"type": "failure",   "message": "missing data"}}
+    msg = {"status": {"type": "failure", "message": "missing data"}}
     return jsonify(msg)
 
   if User.query.filter_by(email=email).count() == 1:
-    msg = {"status": {"type": "failure",   "message": "email already taken"}}
+    msg = {"status": {"type": "failure", "message": "email already taken"}}
     return jsonify(msg)
 
   u = User()
